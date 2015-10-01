2391cda050df2d42d8d74159374bf5ae172fc7dd
john <redford.john.m@gmail.com>
2015-10-01 11:04:42 -0500

    setup for angular

--- server/server.js
+++ server/server.js
@@ -1,8 +1,17 @@
 var express = require('express');
+var path = require('path');
 
 var app = express();
 
 // middleware
+app.use(express.static(path.join(__dirname, './../client')));
+
+// app.get('/', function(req, res) {
+  
+// });
+// app.post('/', function(req, res){
+
+// });
 
 app.listen(3000);
 
